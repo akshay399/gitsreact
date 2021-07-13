152045c323ffe216ceb2ca4593e1a625101b91a0
coldkillerr <yonishitjain123456@gmail.com>
2021-07-14 00:38:10 +0530

    added typescript

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,10 @@
+import Typewriter from "typewriter-effect"
+import { useState } from "react";
+ 
 export const Header = (props) => {
+
+ 
+
   return (
     <header id='header'>
       <div className='intro'>
@@ -9,7 +15,15 @@ export const Header = (props) => {
                 <h2>
                   {props.data ? props.data.title1 : 'Loading'}
                   </h2>
-                  <h2> {props.data ? props.data.title2 : 'Loading'}</h2>
+                  <h2><Typewriter options = {{
+                    autoStart : true,
+                    delay: 40,
+                    strings : [props.data.title2],
+                    loop: true
+                  }}
+                 
+                 /></h2>
+                
                  <h2>{props.data ? props.data.title3 : 'Loading'}</h2>
                   <span></span>
                 
