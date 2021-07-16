b6bb0f15590dfe6577221373644c3373c547e9e3
coldkillerr <yonishitjain123456@gmail.com>
2021-07-16 16:10:32 +0530

    added deployment code in package.json

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -4,10 +4,8 @@ export const Gallery = (props) => {
       <div className='container'>
         <div className='section-title'>
           <h2>Gallery</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.</p>
         </div>
         <div className='row'>
           <div className='portfolio-items'>
